61873884bad59c20a1ba956544cd43cd6330a942
JulioCunh <fcezarf@gmail.com>
2021-07-11 22:26:44 -0300

    3 commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import Apploading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/themes';
 
 import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Apploading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
