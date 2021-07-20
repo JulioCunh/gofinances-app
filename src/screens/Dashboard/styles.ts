9172c720f85b43f1853bfa803cec88c420a45c02
JulioCunh <fcezarf@gmail.com>
2021-07-20 15:22:06 -0300

    5 commit

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -12,7 +12,6 @@ export const Header = styled.View`
   width: 100%;
   height: ${RFPercentage(42)}px;
   background-color: ${({ theme }) => theme.colors.primary};
-
   justify-content: center;
   align-items: flex-start;
   flex-direction: row;
@@ -20,10 +19,12 @@ export const Header = styled.View`
 
 export const UserWrapper = styled.View`
   width: 100%;
+
   padding: 0 24px;
-  margin-top: ${getStatusBarHeight() + RFValue(20)}px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
 `;
 
 export const UserInfo = styled.View`
@@ -60,10 +61,22 @@ export const Icon = styled(Feather)`
 
 export const HighlightCards = styled.ScrollView.attrs({
   horizontal: true,
-  showsVerticalScrollIndicator: false,
+  showsHorizontalScrollIndicator: false,
   contentContainerStyle: { paddingHorizontal: 24 },
 })`
   width: 100%;
   position: absolute;
-  margin-top: ${RFPercentage(22)}px;
+  margin-top: ${RFPercentage(20)}px;
+`;
+
+export const Transactions = styled.View`
+  flex: 1%;
+  padding: 0 24px;
+  margin-top: ${RFPercentage(12)}px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  margin-bottom: 16px;
 `;
